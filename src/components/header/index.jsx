e697e8b94557bc8c3d4ffb6e752ce21a71ea73cb
Liam19950327 <liam.smith19950327@gmail.com>
2025-09-30 04:14:08 -0700

    header complete without animation

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './index.css'
 import headerSvg from '../../assets/header.svg'
 
 const Header = () => {
+    const [flag, setFlag] = useState(true);
     return (
         <div className='header'>
             <div className='header-logo'>
                 <img src={headerSvg} alt="Header" />
+            </div>
+            <div className="hamberger" onClick={() => setFlag(!flag)}>
+                <div className='over1060'>
+                    {flag ?
+                        <span>˃</span>
+                        :
+                        <span>˂</span>
+                    }
+                </div>
+                <div className='under1060'>
+                    {flag ?
+                        <span>˄</span>
+                        :
+                        <span>˅</span>
+                    }
+                </div>
             </div>
         </div>
     )
